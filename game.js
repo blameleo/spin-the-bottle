c8d5233b0cae7e78b21b4b04c7997e889ae9f93e
blameleo <leonardgre@gmail.com>
2023-02-27 19:42:49 +0000

    made final changes

--- game.js
+++ game.js
@@ -1,67 +1,72 @@
-let names=[]
-let addBtn=document.getElementById("addPlayer")
-let input = document.getElementById("names")
-let displayEl= document.getElementById("display")
-let spinEl=document.getElementById("spin")
-let dareEl = document.getElementById("dis")
+let names = [];
+let addBtn = document.getElementById("addPlayer");
+let input = document.getElementById("names");
+let displayEl = document.getElementById("display");
+let spinBtn = document.getElementById("spin");
+let dareEl = document.getElementById("dis");
+const bottle = document.getElementById("bottle");
 
-let angle = 0
-const bottle = document.getElementById("bottle")
+addBtn.addEventListener("click", addNames);
+spinBtn.addEventListener("click", spin);
+function addNames() {
+  if (input.value == "") {
+    document.querySelector(".checker").textContent = "Dont leave input empty";
+  } else {
+    document.querySelector(".checker").textContent = "";
+    names.push(input.value);
+    input.value = "";
 
+    renderList();
+  }
+}
 
-addBtn.addEventListener("click", function(){
-    
-    names.push(input.value)
-    input.value=""
-   
-     renderList()
-    
-})
-
-function renderList(){
-    let listNames = ""
-    for (let i = 0; i < names.length; i++) {
-        listNames += `
+function renderList() {
+  let listNames = "";
+  for (name of names) {
+    listNames += `
             
             <li class="font-bold text-transparent text-lg  bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600 border rounded text-center px-5 pl-0 mb-2 border-pink-500  ">
-              ${names[i]}   
+              ${name}   
             </li>
-        `
-    }
-    displayEl.innerHTML =`
+        `;
+  }
+  displayEl.innerHTML = `
     <div class="text-2xl flex flex-col justify-center items-center  mt-40">
     <h3>Current players :</h3>${listNames} 
     </div>
-    ` 
-    
-
+    `;
 }
 
+function spin() {
+  bottle.classList.add("spin");
 
+  const times = [2000, 2200, 2300, 2400, 2500, 2600, 2700, 2800, 2900];
+  let random = Math.floor(Math.random() * times.length);
 
+  setTimeout(pause, times[random]);
 
- spinEl.addEventListener("click",function(){ 
-let random1=Math.floor(Math.random()*names.length)
-let random2= Math.floor(Math.random()*names.length)
-let player1=names[random1]
-let player2=names[random2]
-    console.log(player1)
-    console.log(player2)
+  bottle.classList.remove("pause");
+}
 
+function pause() {
+  bottle.classList.add("pause");
+  pickName();
+}
 
+function random() {
+  return Math.floor(Math.random() * names.length);
+}
 
-    if(player1!=player2){
-     dareEl.innerHTML = `${player1} dares ${player2}`
+function pickName() {
+  let name1 = names[random()];
+  let name2 = names[random()];
+  if (name1 !== name2) {
+    dareEl.innerHTML = `<p class="fade-in bg-zinc-900 border-t-4 border-t-pink-600 px-3 p-2 rounded text-center ">${name1} <span class="text-red-500">dares</span>  ${name2}</p>`;
+  } else if (name1 == name2) {
+    name1 = names[random()];
+    name2 = names[random()];
+    if (name1 !== name2) {
+      dareEl.innerHTML = `<p class="fade-in bg-zinc-900 border-t-4 border-t-pink-600 px-3 p-2 rounded text-center ">${name1} <span class="text-red-500">dares</span>  ${name2}</p>`;
     }
-
-    
-// angle = angle + Math.random() * 360
-angle = angle + 1000 + Math.floor(Math.random() * 361)
-bottle.style.transform = `rotate(${angle}deg)`
-   
-  
-})
-
-
-
-
+  }
+}
